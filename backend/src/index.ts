34ed19103d71e92012b54ae61f8865c4f7663818
Ronit Kedia <kediaronit.21@gmail.com>
2025-08-30 21:09:36 +0530

    feat(auth): setup better auth in frontend and backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,23 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth";
 
 dotenv.config();
 const app = express();
 
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5001;
