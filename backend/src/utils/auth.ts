d53fd8f275a39fa81fdbf49bae42c07024071916
Ronit Kedia <kediaronit.21@gmail.com>
2025-08-30 21:21:21 +0530

    feat(auth): setup email otp authentication from better-auth

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,9 +1,22 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { emailOTP } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        if (type === 'sign-in') {
+          // TODO: Setup Resend
+        }
+      },
+    }),
+  ],
 });
--- frontend/src/utils/auth-client.ts
+++ frontend/src/utils/auth-client.ts
@@ -1,5 +1,7 @@
+import { emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BACKEND_URL,
+  plugins: [emailOTPClient()],
 });
