2dd3a4848b6799fe3b2e04ff91393cdbc8d2ee1c
Ronit Kedia <kediaronit.21@gmail.com>
2025-08-30 20:07:30 +0530

    feat: add cookie-parser and cors middleware to express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5001;
 
