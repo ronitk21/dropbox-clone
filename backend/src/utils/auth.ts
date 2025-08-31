125ebe2783e65f90ee45dd20c4bb7cc9f6b0f249
Ronit Kedia <kediaronit.21@gmail.com>
2025-08-31 14:37:10 +0530

    feat(auth): setup email verification functionality

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -13,9 +14,25 @@ export const auth = betterAuth({
   plugins: [
     emailOTP({
       async sendVerificationOTP({ email, otp, type }) {
-        if (type === 'sign-in') {
-          // TODO: Setup Resend
+        let subject = "Your Verification OTP";
+        if (type === "sign-in") subject = "Sign-in Verification Code";
+        if (type === "email-verification")
+          subject = "Verify Your Email Address";
+        if (type === "forget-password") subject = "Password Reset Code";
+        if (process.env.NODE_ENV === "development") {
+          console.log("OTP for", email, "=>", otp);
         }
+
+        await resend.emails.send({
+          from: "Auth <noreply@mail.buildwithronit.xyz>",
+          to: email,
+          subject: subject,
+          html: `<div style="font-family:sans-serif;font-size:16px;color:#333">
+                    <p>Your one-time password (OTP) is:</p>
+                    <h2 style="letter-spacing:3px">${otp}</h2>
+                    <p>This code will expire in 10 minutes.</p>
+                </div>`,
+        });
       },
     }),
   ],
